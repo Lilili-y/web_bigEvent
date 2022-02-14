b224670ed5717c6eeeefde8c4f37d508417e267b
Lilili-y <184972241@qq.com>
2022-02-14 19:35:50 +0800

    更改头像功能

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,4 +1,5 @@
 $(function () {
+    var layer = layui.layer;
     // 1.1 获取裁剪区域的 DOM 元素
     var $image = $('#image')
     // 1.2 配置选项
@@ -15,7 +16,17 @@ $(function () {
         $('#btnChooseImage').click();
     })
     $('#btnChooseImage').on('change', function () {
+        // 拿到选择的文件
         var files = this.files;
-        console.log(files);
+        if (files.length == 0) {
+            return layer.msg("未选择文件")
+        }
+        //将文件转换为路径
+        var imgURL = URL.createObjectURL(files[0]);
+        // 3. 重新初始化裁剪区域
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', imgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
     })
 })
